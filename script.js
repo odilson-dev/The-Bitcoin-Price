7054a8ed58935eaa582aa6e4034e4b046330db05
Italis Odilson Woodly <109919684+odilson-coder@users.noreply.github.com>
2023-08-05 13:48:43 -0400

    Add files via upload
    This is the same Project using Fetch witch is more easier to get Data from API

--- script.js
+++ script.js
@@ -3,37 +3,45 @@ const url = "https://blockchain.info/ticker";
 let save = 0;
 
 
-function getPrice(){
-let requete = new XMLHttpRequest();
-let reponse, priceEuro;
+async function getPrice(){
+// let requete = new XMLHttpRequest();
+// let reponse, priceEuro;
 
-requete.open("GET", url);
+// requete.open("GET", url);
 
-requete.responseType = "json";
-requete.send();
+// requete.responseType = "json";
+// requete.send();
 
 
-requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE){
-        if (requete.status === 200){
-            reponse = requete.response;
-             priceEuro = reponse["EUR"]["last"];
-            document.querySelector("#price_label").textContent = priceEuro;
-            if  (priceEuro != save && save != 0 ){
-                console.log("The price has changed");
-            }
+// requete.onload = function() {
+//     if (requete.readyState === XMLHttpRequest.DONE){
+//         if (requete.status === 200){
+//             reponse = requete.response;
+//              priceEuro = reponse["EUR"]["last"];
+//             document.querySelector("#price_label").textContent = priceEuro;
+//             if  (priceEuro != save && save != 0 ){
+//                 console.log("The price has changed");
+//             }
 
-            save = priceEuro
-        ;
+//             save = priceEuro
+//         ;
             
-        } else {
-            console.log("A problem has occurred, please come back later.")
-        }
-    }
+//         } else {
+//             console.log("A problem has occurred, please come back later.")
+//         }
+//     }
 
+// }
+const requete = await fetch(url,{
+    method: "GET"
+});
+if (!requete.ok){
+    alert("A problem has occurred, please come back later!")
+} else{
+    let donnees = await requete.json();
+    document.querySelector("#price_label").textContent = donnees.EUR.last;
 }
 
 }
-
 setInterval(getPrice, 1000);
 
